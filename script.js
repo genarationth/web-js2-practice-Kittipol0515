69aeb689aea1cbfb242b506bcf26b08356991a32
kittipol <kittipol.surapinchai@gmail.com>
2023-03-10 20:06:22 +0700

    Edit homework

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// New contender
+// challenge 1 : Create Age Calculator
+    let birthYear = 1998;
+    let futureYear = 2026;
+    
+    let possibleYear = futureYear - birthYear;
+    console.log(possibleYear);   
+    // Answer 28
+
+// Create an information card
+    const myName = 'Kitti';
+    const myCity = 'Bangkok';
+    const myHobby = 'sleeping in the bed';
+
+    console.log(`Hi,my name is ${myName}.I live in ${myCity} and enjoy ${myHobby}`);
+    // Hi,my name is Kitti.I live in Bangkok and enjoy sleeping in the bed
